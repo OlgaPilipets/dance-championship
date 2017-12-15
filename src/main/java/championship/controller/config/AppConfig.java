7d89d40f688deb43c2596c1eb1fb0e635cd0c7cb
Olga Pilipets <o.pilipets@gmail.com>
2017-12-16 01:57:22 +0300

    #2-spring-configuration-customisation

---
+++ src/main/java/championship/controller/config/AppConfig.java
@@ -0,0 +1,9 @@
+package championship.controller.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"championship"})
+public class AppConfig {
+}
